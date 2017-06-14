32967b9b99f7ca7200f8bd78aa3656fe49267a23
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-06-14 12:30:11 +0500

    added login spider

--- tutorial/spiders/login_spider.py
+++ tutorial/spiders/login_spider.py
@@ -1,11 +1,47 @@
 import scrapy
 import json
+import re
+import os
 
 
-class FccSpider(scrapy.Spider):
+class LoginSpider(scrapy.Spider):
+    ''' Login to restframework.herokuapp.com and parse all the snippets
+        and the write them to rest.json
+    '''
     name = 'login'
 
-    start_urls = ['https://mail.google.com/mail/u/0/#inbox']
+    allowed_domains = ['restframework.herokuapp.com']
+    start_urls = ['http://restframework.herokuapp.com/api-auth/login/']
 
     def parse(self, response):
-        print(response.xpath('//h1[@class="flat-top wrappable"]/text()').extract_first())
+        os.remove('rest.json') 
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'username': 'aziz', 'password': 'aziz'},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        if str.encode("Username") in response.body:
+            self.logger.error("Login failed")
+            return
+        print("Login successful")
+        yield response.follow(url='http://restframework.herokuapp.com/snippets/',
+                              callback=self.parse_snippets)
+
+    def parse_snippets(self, response):
+        f = open('rest.json', 'a')
+        div = response.xpath('//div[@class="response-info"]').extract_first()
+        div = re.sub('<b.*?>(.+?)</b>', '', div)
+        div = re.sub('<span.*?>.*|\n*</span>', '', div)
+        div = re.sub('<pre.*?>', '', div)
+        div = re.sub('</pre>', '', div)
+        div = re.sub('<div.*?>', '', div)
+        div = re.sub('</div>', '', div)
+        div = re.sub('<a.*?>', '', div)
+        div = re.sub('</a>', '', div)
+        json_data = div
+        f.write(json_data)
+        next_link = response.xpath(
+            '//a[@aria-label="Next"]/@href').extract_first()
+        yield response.follow(next_link, callback=self.parse_snippets)

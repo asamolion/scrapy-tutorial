c303cd78558558294017fabab807631a7dd97be6
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-06-14 15:03:54 +0500

    modified ajax spider (still incomplete)

--- tutorial/spiders/ajax_spider.py
+++ tutorial/spiders/ajax_spider.py
@@ -1,13 +1,26 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class AjaxSpider(scrapy.Spider):
     ''' Spider that extracts AJAXed content 
         i.e. dynamically generated content
     '''
+
     name = 'ajax'
 
     start_urls = ['http://quotes.toscrape.com/js/']
 
+    def start_requests(self):
+        '''
+        WILL NEED SPLASH HEADLESS BROWSER FOR THIS TO WORK!
+        '''
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse,
+                                endpoint='render.html',
+                                args={'wait': 3},
+                               )
     def parse(self, response):
+        quotes = response.xpath('//div[@class="quote"]').extract()
+        print(quotes)
         pass
